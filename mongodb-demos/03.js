a698423de25074934ba68b1fd32d96b65e9a1fe5
Paliwal <ashish.paliwal@capgemini.com>
2018-08-31 16:52:53 +0530

    final

--- mongodb-demos/03.js
+++ mongodb-demos/03.js
@@ -2,11 +2,27 @@ const mongoose = require('mongoose');
 const Config = require('./config');
 var uri = Config.MongoURI;
 
-mongoose.connect(uri, { useNewUrlParser: true }).then(()=>{
+//Using Promise
+/*mongoose.connect(uri, { useNewUrlParser: true }).then(()=>{
     var connection = mongoose.connection;
     console.log(`Connected to Database : ${connection.db.databaseName}`);
     connection.close();
     console.log('Connection Closed');
 }).catch((err)=>{
     console.log(`Error : ${err}`);
-});
+});*/
+
+
+//using async await
+(async()=>{
+    try{
+        var mongooseConnection = await mongoose.connect(uri, { useNewUrlParser: true });
+        var connection = mongooseConnection.connection;
+        console.log(`Connected to Database : ${connection.db.databaseName}`);
+        connection.close();
+        console.log('Connection Closed');
+    }
+    catch(err){
+        console.log(`Error : ${err}`);
+    }
+})();
--- mongodb-demos/config.js
+++ mongodb-demos/config.js
@@ -1,4 +1,4 @@
 const Configs = {
-    'MongoURI':'mongodb://127.0.0.1:27017/SampleDB'
+    'MongoURI':'mongodb://localhost/SampleDB'
 }
 module.exports = Configs;
